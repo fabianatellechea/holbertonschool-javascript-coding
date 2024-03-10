bad278de741bc5cd8c67d07dc77f45458772eff7
fabianatellechea <fabianagtellechea@gmail.com>
2024-03-10 15:47:10 -0700

    added: task 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const episode = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/${episode}';
+
+request(url, function (error, content) {
+  if (error) {
+    console.error('error:', error);
+  }
+  console.log(JSON.parse(content.body).title);
+});
