f32b493e492f00dd1fb4051a6dd55d7570e2f5d9
fabianatellechea <fabianagtellechea@gmail.com>
2024-03-10 16:36:43 -0700

    added: task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,11 +3,11 @@ const request = require('request');
 const url = process.argv[2];
 let count = 0;
 
-request(url, function (error, content) {
+request(url, function (error, content, body) {
     if (error) {
       console.error('error:', error);
     }
-  for (const movie of JSON.parse(content).results) {
+  for (const movie of JSON.parse(body).results) {
     for (const c of movie.characters) {
       if (c.includes('18')) {
         count++;
