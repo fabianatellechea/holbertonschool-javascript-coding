2b8e2129b2ecd42bb9991fbadd491c945ee24cd2
fabianatellechea <fabianagtellechea@gmail.com>
2024-03-10 15:29:16 -0700

    added: task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file_path = process.argv[2];
+const filename = process.argv[2];
 
-fs.readFile(file_path, 'utf-8', function (err, data) {
+fs.readFile(filename, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
