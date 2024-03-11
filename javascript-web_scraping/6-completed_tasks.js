415850ed1b2df57d5554aed836fe953d54434a85
fabianatellechea <fabianagtellechea@gmail.com>
2024-03-10 17:13:03 -0700

    added: task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, content, body) {
+  if (error) {
+    console.error('error:', error);
+  }
+  const tasks = {};
+  JSON.parse(body).forEach(task => {
+    if (task.completed === true) {
+      if (!tasks[task.userId]) {
+        tasks[task.userId] = 0;
+      }
+      tasks[task.userId] += 1;
+    }
+  });
+  console.log(tasksCompleted);
+});
